9a317287b0ac9087680a18b0586051b07d7b127d
tajinderchana <64266321+tajinderchana@users.noreply.github.com>
2021-05-15 22:33:04 +0100

    Add files via upload

---
+++ erase-install.sh
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>DontShowWelcome</key>
+    <true/>
+    <key>ForceSignInWindow</key>
+    <true/>
+    <key>HideAbout</key>
+    <true/>
+    <key>HideGetHelp</key>
+    <true/>
+    <key>HideGetSoftware</key>
+    <true/>
+    <key>HidePrefs</key>
+    <true/>
+    <key>HideQuit</key>
+    <true/>
+    <key>LoginLogo</key>
+    <string>/usr/local/images/vc_banner.png</string>
+    <key>MountSharesWithFinder</key>
+    <false/>
+    <key>NetworkCheckAutomatically</key>
+    <true/>
+    <key>OIDCChangePasswordURL</key>
+    <string>https://aka.ms/sspr</string>
+    <key>OIDCProvider</key>
+    <string>Azure</string>
+    <key>OIDCROPGID</key>
+    <string>4fe6f45d-0d45-4faf-a652-f7f8d318d2cb</string>
+    <key>OIDCRedirectURI</key>
+    <string>https://127.0.0.1/jamfconnect</string>
+    <key>OIDCResetPasswordURL</key>
+    <string>https://aka.ms/sspr</string>
+  </dict>
+</plist>
